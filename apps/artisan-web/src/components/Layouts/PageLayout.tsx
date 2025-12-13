2abc4864b3f243a7a777634af962a9d78059a546
yash-devop <yashkamble.dev@gmail.com>
2025-12-13 19:37:56 +0530

    feat: pageLayout

--- apps/artisan-web/src/components/Layouts/PageLayout.tsx
+++ apps/artisan-web/src/components/Layouts/PageLayout.tsx
@@ -1,6 +1,16 @@
 import React from "react";
 import { Outlet } from "react-router";
+import { DocsSidebar } from "../DocsSidebar/DocsSidebar";
 
 export const PageLayout = () => {
-  return <Outlet />;
+  return (
+    <div className="w-full h-screen p-1.5 bg-background">
+      <div className="w-full h-full border border-neutral-400/80 bg-neutral-50 overflow-hidden rounded-lg flex ">
+        <DocsSidebar />
+        <div className=" w-full overflow-auto py-3 ">
+          <Outlet />
+        </div>
+      </div>
+    </div>
+  );
 };
