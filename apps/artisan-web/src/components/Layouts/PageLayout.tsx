b00b87f523ddfb5aefc3eab4ce60e0b4a4dad6fb
yash-devop <yashkamble.dev@gmail.com>
2025-12-14 08:39:21 +0530

    fix: page layout (autoscale)

--- apps/artisan-web/src/components/Layouts/PageLayout.tsx
+++ apps/artisan-web/src/components/Layouts/PageLayout.tsx
@@ -4,13 +4,15 @@ import { DocsSidebar } from "../DocsSidebar/DocsSidebar";
 
 export const PageLayout = () => {
   return (
-    <div className="w-full h-screen p-1.5 bg-background">
-      <div className="w-full h-full border border-neutral-400/80 bg-neutral-50 overflow-hidden rounded-lg flex ">
+    <div className="w-full h-screen px-1.5 pt-1.5 pb-1.5 lg:pb-14 bg-background">
+      {/* <div className="h-14 bg-red-400 w-full">header</div> */}
+      <div className="w-full h-full border border-neutral-400/80 bg-neutral-50 overflow-hidden rounded-lg flex">
         <DocsSidebar />
-        <div className=" w-full overflow-auto py-3 ">
+        <div className="h-full w-full overflow-auto py-10 px-16">
           <Outlet />
         </div>
       </div>
+      <footer className="py-4 flex items-center justify-center">Footer</footer>
     </div>
   );
 };
